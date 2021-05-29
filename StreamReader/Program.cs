57d719c2f6477cc635e2636c4ab339282648f150
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-29 11:07:27 +0530

    Add files via upload

---
+++ StreamReader/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Stream_Reader
+{
+    class Program
+    {
+        public static void ReadfromFile(string filepath)
+        {
+            StreamReader r = new StreamReader("text.txt");
+            while (!r.EndOfStream)
+            {
+                Console.WriteLine(r.ReadLine());
+            }
+            r.Close();
+        }
+         public static void WriteToFile(string filePath)
+        {
+            string[] names = new string[] { "satya", "messi", "curry" };
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                foreach (string s in names)
+                {
+                    sw.WriteLine(s);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            String filePath = "text.txt";
+            WriteToFile(filePath);
+            ReadfromFile(filePath);
+        }
+
+        }
+    }
+
