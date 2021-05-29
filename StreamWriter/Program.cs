306c8751f785f30a6e70937fd79f7d39a3e88b1b
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-29 11:03:13 +0530

    Add files via upload

---
+++ StreamWriter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace Stream_Writer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("demo.txt");
+            string temp = string.Empty;
+            do
+            {
+                Console.WriteLine("Enter your text [-1 to exit];");
+                temp = Console.ReadLine();
+                if (temp != "-1")
+                {
+                    sw.WriteLine(temp);
+                }
+
+            }while(temp != "-1") ;
+            sw.Close();
+                string line;
+                using (StreamReader sr = new StreamReader("demo.txt"))
+
+                {
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            }
+    }
+            
+            
+              
+            
+    
+
